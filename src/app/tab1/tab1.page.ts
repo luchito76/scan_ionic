0222a301e51d97719bd932cd122904391ab9cdfe
LUIS ALBERTO PARADA <luisparadawagner@gmail.com>
2020-04-25 20:21:19 -0300

    Se crea método para scanear QR

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,58 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { BarcodeScanner, BarcodeScannerOptions } from '@ionic-native/barcode-scanner/ngx';
 
 @Component({
-  selector: 'app-tab1',
-  templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+	selector: 'app-tab1',
+	templateUrl: 'tab1.page.html',
+	styleUrls: [ 'tab1.page.scss' ]
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit {
+	itemForm: FormGroup;
 
-  constructor() {}
+	constructor(private barcodeScanner: BarcodeScanner, private fb: FormBuilder) {}
 
+	ngOnInit() {
+		this.initForm();
+	}
+
+	initForm(data?) {
+		let dni: any[] = [];
+		if (data) {
+			dni = data.text.split('@');
+			debugger;
+		}
+
+		this.itemForm = this.fb.group({
+			nombre: [ data ? dni[1] : '' ],
+			apellido: [ data ? dni[2] : '' ],
+			dni: [ data ? dni[4] : '' ]
+		});
+	}
+
+	scan() {
+		const options: BarcodeScannerOptions = {
+			preferFrontCamera: true,
+			showFlipCameraButton: true,
+			showTorchButton: true,
+			torchOn: false,
+			prompt: 'Place a barcode inside the scan area',
+			resultDisplayDuration: 500,
+			formats: 'QR_CODE,PDF_417 ',
+			orientation: 'landscape'
+		};
+
+		this.barcodeScanner
+			.scan(options)
+			.then((barcodeData) => {
+				console.log('Barcode data', barcodeData);
+
+				this.initForm(barcodeData);
+
+				alert('QRR: ' + JSON.stringify(barcodeData));
+			})
+			.catch((err) => {
+				console.log('Error', err);
+			});
+	}
 }
